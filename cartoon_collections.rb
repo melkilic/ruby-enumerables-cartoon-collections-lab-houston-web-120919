83f7b8d757c812861bd6abb7785a7c1007a2f2e3
Melike KILIC <melike180400@gmail.com>
2019-10-29 20:05:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,7 @@ end
 def summon_captain_planet(planeteer_calls)
 planeteer_calls.map! {|name| name.capitalize }
 names.each { |name| puts name + '!' }
+planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
