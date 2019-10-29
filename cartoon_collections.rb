e4f78a88ccdc966495cf079abc1d4ec2579b368e
Melike KILIC <melike180400@gmail.com>
2019-10-29 21:07:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,10 @@ end
 
 
 def long_planeteer_calls(array)
-  array.any? {|word| word.size>4}# code an argument here
-  # Your code here
+  array.any? {|word| word.size>4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
