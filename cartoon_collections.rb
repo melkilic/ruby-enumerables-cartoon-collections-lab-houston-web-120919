cae3240a19df3f156f818a38a23c611412735724
Melike KILIC <melike180400@gmail.com>
2019-10-29 19:46:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves=["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarves.each_with_index{ |item, index|
+dwarves.each_with_index{ |item, index|
   dwarves[item] = index
 }
 dwarves# code an argument here
