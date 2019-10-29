9a03a64fca5e323d6aaee6cd382c7dd33a416958
Melike KILIC <melike180400@gmail.com>
2019-10-29 20:09:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,9 @@ dwarves.each_with_index{ |item, index| puts "#{index + 1}.#{item}"
 end
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls.map! {|name| name.capitalize puts name + '!' }
+planeteer_calls.map! {planeteer_calls << "#{name.capitalize}!" }
 end
-planeteer_calls
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
